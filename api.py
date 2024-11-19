c99b87eefcd2f88680d851d10b730473241482d8
Brian Ramsel <bramsel@localhost.localdomain>
2024-11-18 21:48:13 -0500

    adding locations and cookie

--- api.py
+++ api.py
@@ -15,6 +15,7 @@ def apiGetCs(did,c):
     return x
 
 def apiGetCards(did,s):
+    print(s)
     if type(did) != str:
             return []
     s = s.replace('_',' ').replace('8',"'").replace('9','?')
@@ -25,9 +26,35 @@ def apiGetCards(did,s):
             x += [t[:5]+ ('Yes' if t[5] else 'No',) + t[6:8] + t[-1:]]
         return x
     elif s == 'Monsters':
-        return getAllMonsters(did)
+        tmp = getAllMonsters(did)
+        x = []
+        for t in tmp:
+            x += [t[:5]+ ('Yes' if t[5] else 'No',) + t[6:8] + t[-1:]]
+        return x
     elif s == 'Items':
-        return getAllItems(did)
+        tmp = getAllItems(did)
+        x = []
+        for t in tmp:
+            x += [t[:5]+ ('Yes' if t[5] else 'No',) + t[6:8] + t[-1:]]
+        return x
+    elif s == 'Locations':
+        tmp = getAllLocations(did)
+        x = []
+        for t in tmp:
+            x += [t[:5]+ ('Yes' if t[5] else 'No',) + t[6:8] + t[-1:]]
+        return x
+    elif s == 'BaseSets':
+        st = getBaseSetCards()
+        col = getFullCollection(did)
+        tmp = {}
+        for c in col:
+            if not c[9] in tmp:
+                tmp[c[9]] = []
+            tmp[c[9]] += [[c]]
+        out = []
+        for ss in st:
+            #out += [['', ss[1], 'Yes' if ss[0] in tmp.keys() else 'No', len(tmp.get(ss[0],[]))]]
+            out += [{'name':ss[1], 'has': 'Yes' if ss[0] in tmp.keys() else 'No', 'count': len(tmp.get(ss[0],[])), 'cs': tmp.get(ss[0],[])}]
     else:
         st = getCardsInSetByName(s)
         col = getFullCollection(did)
--- sqlhelper.py
+++ sqlhelper.py
@@ -13,6 +13,12 @@ def getCardsInSetByName(name):
                     " sets on cardsinsets.setid = sets.rwid where sets.name = %s", (name,))
         return cur.fetchall()
 
+def getBaseSetCards():
+    with mydb.db_cursor() as cur:
+        cur.execute("SELECT monid, mons.name from cardsinsets join mons on cardsinsets.monid = mons.rwid join"+
+                    " sets on cardsinsets.setid = sets.rwid where sets.rwid <= 0")
+        return cur.fetchall()
+
 def getMonSets():
     with mydb.db_cursor() as cur:
         cur.execute("SELECT name from sets where rwid > 0 and rwid <50")
@@ -21,6 +27,16 @@ def getMonSets():
 def getItemSets():
     with mydb.db_cursor() as cur:
         cur.execute("SELECT name from sets where rwid >= 50 and rwid <99")
+        return cur.fetchall()
+
+def getLocSets():
+    with mydb.db_cursor() as cur:
+        cur.execute("SELECT name from sets where rwid >= 300 and rwid <400")
+        return cur.fetchall()
+
+def getExpansions():
+    with mydb.db_cursor() as cur:
+        cur.execute("select exp,expansion from expansions join sets on expansions.expansion = sets.name order by sets.rwid")
         return cur.fetchall()
 
 def getAllCollections():
@@ -63,8 +79,29 @@ def getAllItems(user):
                 " where sets.rwid > 0) as c on c.monid = collections.monid where users.did = %s and mons.class = 'Item' ",(user,))
         return cur.fetchall()
 
+def getAllLocations(user):
+    with mydb.db_cursor() as cur:
+        cur.execute("select collections.rwid, mons.cr, mons.name, mons.exp, grade, holo, ed, value, c.name from"+
+                " collections inner join users on collections.uid = users.rwid inner join mons on collections.monid = mons.rwid"+
+                " left join (select monid, setid, name from cardsinsets inner join sets on sets.rwid = cardsinsets.setid"+
+                " where sets.rwid > 0) as c on c.monid = collections.monid where users.did = %s and mons.class = 'Locations'",(user,))
+        return cur.fetchall()
+
 def getAllUsers():
     with mydb.db_cursor() as cur:
         cur.execute("select name, did, count(value), sum(value), gp from users join collections on users.rwid = collections.uid"+
                     " group by (name, did, gp)")
         return cur.fetchall()
+
+def setSession(ses, did):
+    with mydb.db_cursor() as cur:
+        cur.execute("insert into sessions values(%s, %s)", (ses, did))
+    return
+
+def getSession():
+    with mydb.db_cursor() as cur:
+        try:
+            cur.execute("select did from sessions")
+            return cur.fetchone()
+        except:
+            return None
